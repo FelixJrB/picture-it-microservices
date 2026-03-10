afa0446fc22ab60de650d4ccf1c446391e746e57
Felix Berglund <fb222rs@student.lnu.se>
2026-03-10 13:26:35 +0100

    feat: implement server setup and MongoDB connection, add mongoose configuration

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import express from 'express';
-import { connectToMongoDB } from './config/mongoose.js';
-import { router } from './routes/router.js';
+import { connectToMongoDB } from './src/config/mongoose.js';
+import { router } from './src/routes/router.js';
 
 
 await connectToMongoDB('mongodb://localhost:27017/blog');
