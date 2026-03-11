a97bc420ec895ff432f0c4a592251211a79d8a88
Felix Berglund <fb222rs@student.lnu.se>
2026-03-11 16:12:15 +0100

    fix: update MongoDB connection string for auth service

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import { connectToMongoDB } from './src/config/mongoose.js';
 import { router } from './src/routes/router.js';
 
 
-await connectToMongoDB('mongodb://localhost:27017/blog');
+await connectToMongoDB('mongodb://localhost:27018/auth-service');
 
 const app = express();
 const PORT = 3000;
