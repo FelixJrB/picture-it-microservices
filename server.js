df7ed9bae6489218574c6b545c45205b0c319d99
Felix Berglund <fb222rs@student.lnu.se>
2026-03-24 14:40:34 +0100

    feat: mount router at /api/v1/images and add error handler

--- server.js
+++ server.js
@@ -1,15 +1,23 @@
-import express from 'express';
-import { connectToMongoDB } from './src/config/mongoose.js';
-import { router } from './src/routes/api/v1/resourceRoutes.js';
+import express from 'express'
+import { connectToMongoDB } from './src/config/mongoose.js'
+import { router } from './src/routes/api/v1/resourceRoutes.js'
 
 
-await connectToMongoDB('mongodb://localhost:27017/resource-service');
+await connectToMongoDB('mongodb://localhost:27017/resource-service')
 
-const app = express();
-const PORT = 3000;
+const app = express()
+const PORT = 3000 
 
-app.use(express.json({ limit: '500kb'}));
-app.use(router);
+app.use(express.json({ limit: '500kb'}))
+app.use('/api/v1/images', router)
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500).json({
+    status_code: err.status || 500,
+    message: err.message || 'An unexpected condition was encountered.'
+  })
+})
+
 app.listen(PORT, '0.0.0.0', () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+    console.log(`Server is running on port ${PORT}`)
+})
